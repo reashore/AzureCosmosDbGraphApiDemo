f5081acdef2706bd9023e9bad2feec39f0686cd2
Frank J. Reashore <reashore@telus.net>
2018-08-18 18:12:42 -0700

    Add project files.

---
+++ Demos/QueryAirportGraph.cs
@@ -0,0 +1,148 @@
+﻿using Gremlin.Net.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace CosmosDb.GremlinApi.Demos
+{
+	public static class QueryAirportGraph
+	{
+		private const int Port = 443;
+		private const string DatabaseName = "GraphDb";
+		private const string GraphName = "Airport";
+
+		public static async Task Run()
+		{
+			Debugger.Break();
+
+			string hostname = ConfigurationManager.AppSettings["CosmosDbHostName"];
+			string masterKey = ConfigurationManager.AppSettings["CosmosDbMasterKey"];
+			string username = $"/dbs/{DatabaseName}/colls/{GraphName}";
+
+			GremlinServer gremlinServer = new GremlinServer(hostname, Port, true, username, masterKey);
+
+			using (GremlinClient client = new GremlinClient(gremlinServer))
+			{
+				Console.WriteLine();
+				Console.WriteLine("*** Scenario 1 - First eat (> .3 rating), then switch terminals, then go to gate ***");
+
+				const string firstEatThenSwitchTerminals = @"
+					// Start at T1, Gate 2
+						g.V('Gate T1-2')
+
+					// Traverse edge from gate to restaurants
+						.outE('gateToRestaurant')
+						.inV()
+
+					// Filter for restaurants with a rating higher than .3
+						.has('rating', gt(0.3))
+
+					// Traverse edge from restaurant back to terminal (T1)
+						.outE('restaurantToTerminal')
+						.inV()
+					
+					// Traverse edge from terminal to next terminal (T2)
+						.outE('terminalToNextTerminal')
+						.inV()
+					
+					// Traverse edge from terminal (T2) to gates
+						.outE('terminalToGate')
+						.inV()
+					
+					// Filter for destination gate T2, Gate 3
+						.has('id', 'Gate T2-3')
+					
+					// Show the possible paths
+						.path()";
+
+				await RunAirportQuery(client, firstEatThenSwitchTerminals);
+
+				Console.WriteLine();
+				Console.WriteLine("*** Scenario 2 - First switch terminals, then eat (> .2 rating), then go to gate ***");
+
+				const string firstSwitchTerminalsThenEat = @"
+					// Start at T1, Gate 2
+						g.V('Gate T1-2')
+
+					// Traverse edge from gate to terminal T1
+						.outE('gateToTerminal')
+						.inV()
+
+					// Traverse edge from terminal to next terminal (T2)
+						.outE('terminalToNextTerminal')
+						.inV()
+
+					// Traverse edge from terminal to restaurants
+						.outE('terminalToRestaurant')
+						.inV()
+					
+					// Filter for restaurants with a rating higher than .2
+						.has('rating', gt(0.2))
+					
+					// Traverse edge from restaurant back to gates
+						.outE('restaurantToGate')
+						.inV()
+					
+					// Filter for destination gate T2, Gate 3
+						.has('id', 'Gate T2-3')
+					
+					// Show the possible paths
+						.path()";
+
+				await RunAirportQuery(client, firstSwitchTerminalsThenEat);
+			}
+		}
+
+		private static async Task RunAirportQuery(IGremlinClient client, string gremlinCode)
+		{
+			IReadOnlyCollection<dynamic> results = await client.SubmitAsync<dynamic>(gremlinCode);
+
+			int count = 0;
+
+			foreach (dynamic result in results)
+			{
+				count++;
+				dynamic jResult = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(result));
+				JArray steps = (JArray)jResult["objects"];
+
+				int userStep = 0;
+				int totalDistanceInMinutes = 0;
+				int i = 0;
+
+				Console.WriteLine();
+				Console.WriteLine($"Choice # {count}");
+
+				foreach (JToken step in steps)
+				{
+					i++;
+					if (step["type"].Value<string>() == "vertex")
+					{
+						userStep++;
+						string userStepCaption = (userStep == 1 ? "Start at" : (i == steps.Count ? "Arrive at" : "Go to"));
+						string vertexInfo = $"{userStep}. {userStepCaption} {step["label"]} = {step["id"]}";
+
+						if (step["label"].Value<string>() == "restaurant")
+						{
+							vertexInfo += $", rating = {step["properties"]["rating"][0]["value"]}";
+							vertexInfo += $", avg price = {step["properties"]["averagePrice"][0]["value"]}";
+						}
+
+						vertexInfo += $" ({totalDistanceInMinutes} min)";
+						Console.WriteLine(vertexInfo);
+					}
+					else
+					{
+						int distanceInMinutes = step["properties"]["distanceInMinutes"].Value<int>();
+						totalDistanceInMinutes += distanceInMinutes;
+						string edgeInfo = $"    ({step["label"]} = {distanceInMinutes} min)";
+						Console.WriteLine(edgeInfo);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using CosmosDb.GremlinApi.Demos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CosmosDb.GremlinApi
+{
+	public static class Program
+	{
+		private static IDictionary<string, Func<Task>> _demoMethods;
+
+		//public static object PopulateAirportGraphAlt { get; }
+
+		private static void Main()
+		{
+			_demoMethods = new Dictionary<string, Func<Task>>
+			{
+				{ "PA", PopulateAirportGraph.Run },
+				{ "QA", QueryAirportGraph.Run },
+			};
+
+			Task.Run(async () =>
+			{
+				ShowMenu();
+				while (true)
+				{
+					Console.Write("Selection: ");
+					string input = Console.ReadLine();
+				    if (string.IsNullOrEmpty(input))
+				    {
+				        continue;
+				    }
+
+				    string demoId = input.ToUpper().Trim();
+
+					if (_demoMethods.Keys.Contains(demoId))
+					{
+						Func<Task> demoMethod = _demoMethods[demoId];
+						await RunDemo(demoMethod);
+					}
+					else if (demoId == "Q")
+					{
+						break;
+					}
+					else
+					{
+						Console.WriteLine($"?{input}");
+					}
+				}
+			}).Wait();
+		}
+
+		private static void ShowMenu()
+		{
+		    Console.WriteLine(@"Cosmos DB Gremlin .NET Demos:");
+		    Console.WriteLine(@"PA Populate airport graph");
+		    Console.WriteLine(@"QA Query airport graph");
+		    Console.WriteLine(@"Q  Quit");
+		}
+
+		private static async Task RunDemo(Func<Task> demoMethod)
+		{
+			try
+			{
+				await demoMethod();
+			}
+			catch (Exception exception)
+			{
+				string message = exception.Message;
+
+				while (exception.InnerException != null)
+				{
+					exception = exception.InnerException;
+					message += Environment.NewLine + exception.Message;
+				}
+
+				Console.WriteLine($"Exception message: {message}");
+			}
+			Console.WriteLine();
+			Console.Write("Done. Press any key to continue...");
+			Console.ReadKey(true);
+			Console.Clear();
+			ShowMenu();
+		}
+	}
+}
